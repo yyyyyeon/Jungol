1a914a0b1aa56157fe47340e4585a0866e250a66
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-26 16:58:13 +0900

    정올 510번 문제

---
+++ Jungol/src/q510/Main.java
@@ -0,0 +1,11 @@
+package q510;
+
+public class Main {
+	public static void main(String[] args) {
+		int var = -1;
+		System.out.println(var);
+		int var2 = 100;
+		System.out.println(var2);
+
+}
+	}
