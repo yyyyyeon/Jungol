40670edbc320beb0fa4bb278bce5530d686318b9
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-26 17:18:06 +0900

    정올 511 문제

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,15 @@
+package q511;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 55;
+		int b = 10;
+		System.out.println (a + " - " + b + " = " + (a-b));
+		
+		a = 2008;
+		b = 1999;
+		
+		System.out.println (a + " - " + b + " = " + (a-b));
+		
+}
+	}
