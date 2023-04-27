62d925640cf62bfcc2acfe41a5238335df5af9b0
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-27 10:08:02 +0900

    정올 508번 문제

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,10 @@
+package q508;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.printf("%10s%10s%10s\n","item","count","price");
+		System.out.printf("%10s%10s%10s\n","pen","20","100");
+		System.out.printf("%10s%10s%10s\n","note","5","95");
+		System.out.printf("%10s%10s%10s\n","eraser","110","97");
+}
+	}
