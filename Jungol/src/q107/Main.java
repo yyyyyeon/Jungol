15dffb44816270018f3a558a9d0b1ed5dc0befeb
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-27 17:37:29 +0900

    정올 107번 문제

---
+++ Jungol/src/q107/Main.java
@@ -0,0 +1,14 @@
+package q107;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double a = 80.50;
+		double b = 22.34;
+		double c = 102.84;
+		System.out.printf("%10.2f",a);
+		System.out.printf("%10.2f",b);
+		System.out.printf("%10.2f",c);
+	}
+
+}
