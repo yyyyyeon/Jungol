1c2003de24cce6342c931c3d9af184ca2c28ff78
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-27 11:57:35 +0900

    정올 9006번 문제

---
+++ Jungol/src/q9006/Main.java
@@ -0,0 +1,13 @@
+package q9006;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("수를 출력할 때는 따옴표를 생략합니다.");
+		System.out.println("123");
+		
+		double d1 =6.5;
+		System.out.println(d1);
+		System.out.printf("%f\n",d1);
+	}
+}
+
