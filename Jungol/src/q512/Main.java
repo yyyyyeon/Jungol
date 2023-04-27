f0dea75f81abce0e541eec4c3cf3a6ce7fa0c13e
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-27 13:55:57 +0900

    정올 512번 문제

---
+++ Jungol/src/q512/Main.java
@@ -0,0 +1,11 @@
+package q512;
+
+public class Main {
+	public static void main(String[] args) {
+		int weight = 49;
+		double grayvity = 0.2683;
+		
+		System.out.printf("%d * %f = %f",weight, grayvity,(weight*grayvity));
+		
+}
+}
