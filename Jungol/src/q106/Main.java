6c0b8ad68a8e9d59d7b5d7789ce548dfa328947f
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-27 17:08:11 +0900

    정올 106번 문제

---
+++ Jungol/src/q106/Main.java
@@ -0,0 +1,10 @@
+package q106;
+
+public class Main {
+	public static void main(String[] args) {
+		int  A = 10;
+		int  B = 20;
+		int  C = 30;
+		System.out.println(A + " + " + B + " = " + C); 
+}
+}
