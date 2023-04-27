6a03c7b849ec440033db875ce4eeed3a80332265
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-27 16:49:54 +0900

    정올 513번 문제

---
+++ Jungol/src/q513/Main.java
@@ -0,0 +1,15 @@
+package q513;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double yard= 91.44;
+		double inch = 2.54;
+		
+		System.out.printf(" 2.1yd = %.1fcm\n",(2.1*yard));
+		System.out.printf("10.5in =  %.1fcm\n",(10.5*inch));
+
+	}
+
+}
