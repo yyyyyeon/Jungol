b3de3f36e5e49e12146548a3cab14b7f816ded5d
Class01 <Class01@DESKTOP-CN96AQ4>
2023-04-28 09:42:19 +0900

    정올 9008번 문제

---
+++ Jungol/src/q9008/Main.java
@@ -0,0 +1,16 @@
+package q9008;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(" subject score");
+		System.out.println("==============");
+		System.out.println("  korean    90");
+		System.out.println(" english   100");
+		System.out.println("computer    80");
+		
+		
+
+	}
+
+}
