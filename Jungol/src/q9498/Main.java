a221e3f8ac9d97a6daf7db87bc56773345d8e9ad
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-01 16:41:09 +0900

    정올 9498번 문제

---
+++ Jungol/src/q9498/Main.java
@@ -0,0 +1,14 @@
+package q9498;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in); 
+		int score = sc.nextInt();
+		sc.close();
+		if (score >= 90) {
+			System.out.println("A");
+		}
+}
+}
