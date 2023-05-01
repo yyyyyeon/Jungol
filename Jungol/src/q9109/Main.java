00ae0553212e089609bb8bd7de06726b5335bc4a
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-01 15:53:27 +0900

    정올 9109 문제

---
+++ Jungol/src/q9109/Main.java
@@ -0,0 +1,15 @@
+package q9109;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		String B = sc.nextLine();
+		sc.close();
+		System.out.print(B);
+
+	}
+
+}
