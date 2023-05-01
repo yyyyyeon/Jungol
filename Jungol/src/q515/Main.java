d963d8aa012d10cda5695ebba78b353a5e59a1af
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-01 17:32:54 +0900

    정올 515번 문제

---
+++ Jungol/src/q515/Main.java
@@ -0,0 +1,14 @@
+package q515;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		System.out.println(a + " * " + b + " = " + (a*b));
+		System.out.println(a + " / " + b + " = " + (a/b));
+}
+}
