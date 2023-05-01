040918501f29dbffff7f713a9b4e43d9568c294a
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-01 10:36:08 +0900

    정올 514번 문제

---
+++ Jungol/src/q514/Main.java
@@ -0,0 +1,13 @@
+package q514;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("height = ");
+		int height = sc.nextInt();
+		sc.close();
+		System.out.println("Your height is " + height + "cm.");
+}
+}
