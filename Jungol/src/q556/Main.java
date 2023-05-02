846bd9623a6b6d2de245c90073dd966c1c0aa8d5
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-02 14:40:43 +0900

    정올 556번 문제

---
+++ Jungol/src/q556/Main.java
@@ -0,0 +1,10 @@
+package q556;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" " );
+		}
+	}
+}
