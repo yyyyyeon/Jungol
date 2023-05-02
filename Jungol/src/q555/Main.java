b34fc3f0045e45d05e1cf0916eff0bf89597b2d0
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-02 14:19:55 +0900

    정올 555번 문제

---
+++ Jungol/src/q555/Main.java
@@ -0,0 +1,18 @@
+package q555;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] ar = new char[10];
+		for (int i = 0; i < ar.length; i++) {
+			ar [i] = sc.next().charAt(0);
+		}
+		for (int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i]);
+		}
+	}
+
+}
