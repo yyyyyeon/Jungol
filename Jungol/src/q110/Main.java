393069bc5de6afe908edb5ac9688c912dc9e2f93
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-03 14:31:50 +0900

    정올 110번 문제

---
+++ Jungol/src/q110/Main.java
@@ -0,0 +1,16 @@
+package q110;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.printf("yard? ");
+		double a = sc.nextDouble();
+		sc.close();
+		System.out.print(a + "yard = ");
+		System.out.printf("%.1f%s",(a*91.44),"cm");
+	}
+
+}
