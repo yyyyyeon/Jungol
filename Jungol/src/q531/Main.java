759f04c11fc3734298e35d779d2fe04dcd8b6b7e
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-03 10:58:15 +0900

    정올 531번 문제

---
+++ Jungol/src/q531/Main.java
@@ -0,0 +1,23 @@
+package q531;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double wehight = sc.nextDouble();
+		sc.close();
+		if (wehight <= 50.80) {
+			System.out.println("Flyweight");
+		} else if (wehight <= 61.23) {
+			System.out.println("Lightweight");
+		} else if (wehight <= 72.57) {
+			System.out.println("Middleweight");
+		} else if (wehight <= 88.45) {
+			System.out.println("Cruiserweight");
+		} else {
+			System.out.println("Heavyweight");
+		}
+	}
+}
