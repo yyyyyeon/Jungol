dbe9afbf2d93dbf8ea87d5879126ee58dbfa9ba4
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-03 17:52:36 +0900

    정올 594번 문제

---
+++ Jungol/src/q594/Main.java
@@ -0,0 +1,17 @@
+package q594;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		String a =sc.next();
+		sc.close();
+		System.out.println(a+a);
+		for(int i =1;i<=10; i++) {
+			System.out.print(a);
+		}
+	}
+
+}
