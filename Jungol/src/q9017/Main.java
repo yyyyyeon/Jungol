8afa9d896c11550eda1a331e37aa9c718dcd3988
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-04 13:48:37 +0900

    정올 9017번 문제

---
+++ Jungol/src/q9017/Main.java
@@ -0,0 +1,17 @@
+package q9017;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 개의 실수를 입력하시오.");
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		System.out.printf("x" + " = %.2f\n", a);
+		System.out.printf("y" + " = %.2f", b);
+
+	}
+
+}
