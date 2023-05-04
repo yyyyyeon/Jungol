486137edb17b0ffb7b97c8b9a096230a5045566d
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-04 16:19:35 +0900

    정올 9010번 문제

---
+++ Jungol/src/q9010/Main.java
@@ -0,0 +1,15 @@
+package q9010;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.print(b+" ");
+		System.out.print(a+"");
+		
+	
+
+	}
+
+}
