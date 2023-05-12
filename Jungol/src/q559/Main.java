71f123f2337d831c5eb3741a789c932b87ae16a7
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-12 14:00:23 +0900

    정올 559번 문제

---
+++ Jungol/src/q559/Main.java
@@ -0,0 +1,21 @@
+package q559;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int b= sc.nextInt();
+		int c= sc.nextInt();
+		double [] a = {85.6, 79.5, 83.1, 80.0, 78.2, 75.0};
+		double sum = (a[b-1]+a[c-1]);
+		System.out.printf("%.1f",sum);
+			
+		
+		
+			
+			
+	}
+
+}
