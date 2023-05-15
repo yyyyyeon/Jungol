d2a0252c770332ed807975c27dfd4bc7eda5f26a
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-15 13:51:18 +0900

    정올 535번 문제

---
+++ Jungol/src/q535/Main.java
@@ -0,0 +1,38 @@
+package q535;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		double a = sc.nextDouble();
+		
+		switch((int)a) {
+		case(int) 4:
+			System.out.println("scholarship");
+		break;
+		case(int) 3:
+			System.out.println("next semester");
+		break;
+		case(int) 2:
+			System.out.println("seasonal semester");
+		break;
+		default:
+			System.out.println("retake");
+			break;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		} 
+		
+	}
+
+}
