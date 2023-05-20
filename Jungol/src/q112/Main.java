8311d844a22b778e5e0d264ee19b4167ffd63e8f
user <user@DESKTOP-N034PUN>
2023-05-20 10:49:35 +0900

    정올 112번 문제

---
+++ Jungol/src/q112/Main.java
@@ -0,0 +1,16 @@
+package q112;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		System.out.print(a + " / " + b + " = " + (a / b) + "..." + (a % b));
+
+	}
+
+}
