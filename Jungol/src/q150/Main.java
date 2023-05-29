bcc3ac58fa96009302f23de9a735cd37721d130d
user <user@DESKTOP-N034PUN>
2023-05-29 11:34:07 +0900

    정올 150번 문제

---
+++ Jungol/src/q150/Main.java
@@ -0,0 +1,19 @@
+package q150;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		String a[]=new String [10];
+		for(int i=0; i<10; i++) {
+			String b =sc.next();
+				a[i]=b;			
+		}
+		for(int i=a.length-1; i>=0; i--) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
